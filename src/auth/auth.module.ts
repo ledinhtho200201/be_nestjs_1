356fe6906f253c16fded8b7d724a5cb05ee576a1
JRabbit <ledinhtho686@gmail.com>
2024-06-12 21:17:13 +0700

    local strategy

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './passport/local.strategy';
+import { UsersModule } from 'src/users/users.module';
+
+@Module({
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
+})
+export class AuthModule { }
