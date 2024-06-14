b6ce04c50d393a7974c89c2bb8605fa467fb7496
JRabbit <ledinhtho686@gmail.com>
2024-06-15 00:56:56 +0700

    create a company

--- src/companies/schemas/company.schema.ts
+++ src/companies/schemas/company.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type CompanyDocument = HydratedDocument<Company>;
 
@@ -12,23 +12,23 @@ export class Company {
     address: string;
 
     @Prop()
-    description: number;
+    description: string;
 
-    @Prop()
+    @Prop({ type: Object })
     createdBy: {
-        _id: string;
+        _id: mongoose.Schema.Types.ObjectId;
         email: string;
     }
 
     @Prop({ type: Object })
     updatedBy: {
-        _id: string;
+        _id: mongoose.Schema.Types.ObjectId;
         email: string;
     }
 
     @Prop({ type: Object })
     deletedBy: {
-        _id: string;
+        _id: mongoose.Schema.Types.ObjectId;
         email: string;
     }
 
