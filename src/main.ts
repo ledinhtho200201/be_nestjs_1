0048e4d7f6917698de9441e5a9fcb8dfa5178a49
JRabbit <ledinhtho686@gmail.com>
2024-06-15 21:17:49 +0700

    transform response, customize message(decorator)

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,20 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
+
   app.useStaticAssets(join(__dirname, '..', 'public')); // access js, css, images
   app.setBaseViewsDir(join(__dirname, '..', 'views')); // store view engine (html)
   app.setViewEngine('ejs');
   app.useGlobalPipes(new ValidationPipe());
+
   // congig cors
   app.enableCors(
     {
