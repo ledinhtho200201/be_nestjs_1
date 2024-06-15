63d19a142cacc5b99e6311614aad75fae20e055b
JRabbit <ledinhtho686@gmail.com>
2024-06-15 11:08:17 +0700

    update user type jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,24 +14,4 @@ export class AppController {
     private configService: ConfigService,
     private authService: AuthService
   ) { }
-
-  @Public()
-  @UseGuards(LocalAuthGuard)
-  @Post('/login')
-  handleLogin(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
-  // @UseGuards(JwtAuthGuard)
-  @Public()
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
-
-  // @UseGuards(JwtAuthGuard)
-  @Get('profile1')
-  getProfile1(@Request() req) {
-    return req.user;
-  }
 }
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Post, Render, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Public } from 'src/decorator/customize';
+import { LocalAuthGuard } from './local-auth.guard';
+
+
+@Controller("auth")
+export class AuthController {
+    constructor(
+        private authService: AuthService
+    ) { }
+
+    @Public()
+    @UseGuards(LocalAuthGuard)
+    @Post('/login')
+    handleLogin(@Request() req) {
+        return this.authService.login(req.user);
+    }
+
+    // @UseGuards(JwtAuthGuard)
+    @Public()
+    @Get('profile')
+    getProfile(@Request() req) {
+        return req.user;
+    }
+
+    // @UseGuards(JwtAuthGuard)
+    @Get('profile1')
+    getProfile1(@Request() req) {
+        return req.user;
+    }
+}
