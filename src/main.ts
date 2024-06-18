4888ba1fd43c825c35e83349f060408b7dc171c6
JRabbit <ledinhtho686@gmail.com>
2024-06-18 16:33:21 +0700

    fix params api, config cors

--- src/main.ts
+++ src/main.ts
@@ -26,9 +26,10 @@ async function bootstrap() {
   // congig cors
   app.enableCors(
     {
-      "origin": "*",
+      "origin": true,
       "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
       "preflightContinue": false,
+      credentials: true
     }
   );
 
